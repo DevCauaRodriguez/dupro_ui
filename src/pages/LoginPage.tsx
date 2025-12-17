9a9bf567ec9bb8d98238619b3b1188d9abb61860
Caua Aquiles <caua.rodrigues@duconseguros.com.br>
2025-12-17 15:13:51 -0300

    tailwinds e vite configurados

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 function LoginPage() {
 return <div className="mb-8">
-    <h1 className="text-4x1 font-bold text-blue-600 mb-2">LOGIN</h1>
+    <h1 className="text-2xl font-bold text-blue-500">LOGiN</h1>
     <p></p>
 </div>
 };
